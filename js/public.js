c8e5442fef8b23d6db23228438ca7ffa96655ecc
張欽揚 <zhangqinyang@zhangqinyangdeMacBook-Pro.local>
2020-11-23 15:09:42 +0800

    v2

--- js/public.js
+++ js/public.js
@@ -5,50 +5,107 @@
  * @link        https://www.ioa.tw/
  */
 
+/*jslint browser: true*/
+/*global $, window, console*/
 $(function () {
-
+	'use strict';
+	/* loading page start */
     window.setTimeout(function() {
-        $('.loading_page').hide();
-    }, 12000);
+        $('.loading_page').fadeOut(1000);
+    }, 18000);
 
     window.setInterval(function() {
         $('.arrow-right').click();
-    }, 2000);
+    }, 3000);
 
-    $('.loading_page_btn').on('click', function() {
-        $('.loading_page').hide();
-    })
+    $('.loading_page_btn').on('click', function () {
+        $('.loading_page').fadeOut(1000);
+    });
 
-    $('.arrow-right').on('click',function() {
-        var count = $('.loading_page_content').children().length
-        var index = $('.loading_page_content').find('.show').index();
-        if((index + 1) == count) {
+    $('.arrow-right').on('click', function () {
+        var count = $('.loading_page_content').children().length,
+			index = $('.loading_page_content').find('.show').index();
+		
+        if ((index + 1) === count) {
             index = -1;
         }
         $('.loading_page_content').find('p').removeClass('show');
-        $('.loading_page_content').find('p').eq(++index).addClass('show');
+        $('.loading_page_content').find('p').eq((index + 1)).addClass('show');
 
         
-    })
+    });
 
-    $('.arrow-left').on('click',function() {
-        var count = $('.loading_page_content').children().length
-        var index = $('.loading_page_content').find('.show').index();
-        console.log(count)
-        console.log(index)
-        if((index - 1) < 0) {
+    $('.arrow-left').on('click', function () {
+        var count = $('.loading_page_content').children().length,
+			index = $('.loading_page_content').find('.show').index();
+        
+        if ((index - 1) < 0) {
             index = count;
         }
 
         $('.loading_page_content').find('p').removeClass('show');
-        $('.loading_page_content').find('p').eq(--index).addClass('show');
+        $('.loading_page_content').find('p').eq(index - 1).addClass('show');
 
         
-    })
-
+    });
+	
+	/* loading page end */
+	
+	/* aside r-p start */
+	
+	var banner = $('#banner').offset().top,
+		sea = $('#sea').offset().top,
+		history = $('#history').offset().top;
+	
+	$(window).scroll(function () {
+		var scrollTop = $(window).scrollTop();
+		if (scrollTop > banner && scrollTop < sea) {
+			$('.r-p').find('a').removeClass('active');
+			$('.r-p').find('a[href="#banner"]').addClass('active');
+		} else if (scrollTop > sea && scrollTop < history) {
+			$('.r-p').find('a').removeClass('active');
+			$('.r-p').find('a[href="#sea"]').addClass('active');
+		} else if (scrollTop > history) {
+			$('.r-p').find('a').removeClass('active');
+			$('.r-p').find('a[href="#history"]').addClass('active');
+		}
+	});
+	/* aside r-p end */
+	
+	/* banner map start */
+	$('.map-icon.hover').mouseenter(function () {
+		var index = $(this).index();
+		var active = $('.map-icon.active').index();
+		if (index !== active) {
+			$(this).find('img').attr('src', '../img/map_icon02.png');	
+		}
+	});
+	
+	$('.map-icon.hover').mouseleave(function () {
+		var index = $(this).index();
+		var active = $('.map-icon.active').index();
+		if (index !== active) {
+			$(this).find('img').attr('src', '../img/map_icon01.png');
+		}
+		
+	});
+	
+	$('.map-icon').on('click', function () {
+		$('.map-icon').removeClass('active');
+		$('.map-icon').find('img').attr('src', '../img/map_icon01.png');
+		$(this).addClass('active');		
+		$(this).find('img').attr('src', '../img/map_icon03.png');
+		
+		var id = $(this).attr('data-id');
+		$('.banner-text').find('.text-box').removeClass('active');
+		$('.banner-text').find('[data-id='+id+']').addClass('active');
+	});
+	
+	$('.map-icon').eq(0).click();
+	/* banner map end */
     $('ul#time-line li').each(function () {
-        var stop = $(window).scrollTop() + $(window).height() / 1.2;
-        var litop = $(this).offset().top;
+        var stop = $(window).scrollTop() + $(window).height() / 1.2,
+			litop = $(this).offset().top;
         if (stop > litop) {
             $(this).addClass('visibility');
         }
@@ -70,13 +127,13 @@ $(function () {
 
     $(".history-content").scroll(function () {
         $('ul#time-line li').each(function () {
-            var stop = $(window).scrollTop() + $(window).height() / 1.2;
-            var litop = $(this).offset().top;
+            var stop = $(window).scrollTop() + $(window).height() / 1.2,
+				litop = $(this).offset().top;
             if (stop > litop) {
                 $(this).addClass('visibility');
             } else {
                 $(this).removeClass('visibility');
-            };
+            }
         });
     });
 });
